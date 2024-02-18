df7549e8f9fdd85bb07325b7c2a7b7f25e6a8cfc
Ayberk Atalay <105879357+AyberkAtalay0@users.noreply.github.com>
2024-02-18 21:25:39 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,21 @@
+from requests import post
+import base64 as b64, cv2
+from time import sleep
+
+import mss
+from mss.tools import to_png as to_bytes
+
+def mainloop(fps):
+  while 1:
+    sleep(1/fps)
+    try:
+      with mss.mss() as visual:
+        monitor = visual.monitors[1]
+        img = visual.grab(monitor)
+        raw_bytes = to_bytes(img.rgb, im.size)
+        print("raw_bytes:", str(raw_bytes)[:15])
+    except Exception as e: print(str(e))
+    print("loop")
+
+if __name__ == "__main__":
+  mainloop(fps=12)

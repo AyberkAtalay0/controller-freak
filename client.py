87c83b4679d4453d588fb83c05e849ccc7930429
Ayberk Atalay <105879357+AyberkAtalay0@users.noreply.github.com>
2024-02-18 23:04:05 +0300

    Update client.py

--- client.py
+++ client.py
@@ -24,8 +24,8 @@ def mainloop(fps):
       with mss.mss() as visual:
         img = visual.grab(visual.monitors[1])
         raw_bytes = to_bytes(img.rgb, img.size)
-        print(raw_bytes)
-        post("https://127.0.0.1", json={"bytes": raw_bytes})
+        print(type(raw_bytes))
+        post("https://127.0.0.1", json={"bytes": str(raw_bytes)})
     except Exception as e: print(str(e))
 
 if __name__ == "__main__":

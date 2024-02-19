10a56ab0cf9872b3086110edd70016bd1ebcd852
Ayberk Atalay <105879357+AyberkAtalay0@users.noreply.github.com>
2024-02-19 23:21:47 +0300

    Update client1.py

--- client1.py
+++ client1.py
@@ -26,7 +26,7 @@ def mainloop(fps):
       with mss() as visual:
         image = visual.grab(visual.monitors[1])
         name = hex(getnode())
-        post(url="http://127.0.0.1:5000/stream", json={"bytes": to_png(image.rgb, image.size), "name": str(name)}, verify=False)
+        post(url="http://127.0.0.1:5000/stream", json={"pixels": str(image.pixels), "name": str(name)}, verify=False)
         print("Image sent to:", str(name))
     except Exception as e: print(str(e))
     sleep(1/fps)

